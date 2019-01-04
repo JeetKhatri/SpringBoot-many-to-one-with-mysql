f6629b12f1edbe616dc9f722cc7482f2147fae53
JeetKhatri <jeet.g.khatri@gmail.com>
2019-01-04 19:23:48 +0530

    cronjob added

--- src/main/java/com/springboot/SpringBootBasicWithSqlDb1Application.java
+++ src/main/java/com/springboot/SpringBootBasicWithSqlDb1Application.java
@@ -2,6 +2,7 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /***
  * 
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootBasicWithSqlDb1Application {
 
 	public static void main(String[] args) {
